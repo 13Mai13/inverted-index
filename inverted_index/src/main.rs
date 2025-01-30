72a3d2824040f817980a55b98951878fdfb00997
mai <maialenberrondo13@gmail.com>
2025-01-30 15:30:01 +0100

    chore: understand how basic hello world works

--- inverted_index/src/main.rs
+++ inverted_index/src/main.rs
@@ -5,10 +5,23 @@ pub fn create_greeting(name: &str) -> String {
 fn main() {
     println!("What's your name?");
     
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    
-    println!("{}", create_greeting(input.trim()));
+    let mut input = String::new(); // creates an empty string to store input
+
+    let stdin = std::io::stdin(); // standar input handler
+
+    let result = stdin.read_line(&mut input);
+
+    match result {
+        Ok(_number_of_bytes_read) => { // _number_of_bytes_read because when read_line it returns the  number of bytes that were read
+            // _ in fron of a var is rust is we won't use it
+            println!("{}", create_greeting(input.trim()));
+        }
+        Err(error) => { // => is used to separate the pattern from the code in match
+            println!("Failed to read line: {}", error)
+        }
+    }
+    // Efficient way: 
+    //std::io::stdin()
+    //     .read_line(&mut input)
+    //     .expect("Failed to read line");
 }
