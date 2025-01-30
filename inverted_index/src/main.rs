2f5b57e108b6a01c66bbd3493eef5d1d496c2b00
mai <maialenberrondo13@gmail.com>
2025-01-30 14:29:39 +0100

    feat: add hello mai

--- inverted_index/src/main.rs
+++ inverted_index/src/main.rs
@@ -1,3 +1,14 @@
+pub fn create_greeting(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("What's your name?");
+    
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    
+    println!("{}", create_greeting(input.trim()));
 }
